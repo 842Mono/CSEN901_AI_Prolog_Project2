b0bc6c1be66c8ef30747093eeb5b89f91e5d0890
SallyHabib <sally.hanen@student.guc.edu.eg>
2018-11-14 16:43:38 +0200

    solving conflicts

--- main.pl
+++ main.pl
@@ -1,30 +1,18 @@
-posObst(1, 1).
-posDS(0, 2).
-
-posWW(0, 0, s0).
-posWW(0, 1, s0).
-posJon(2, 2, 0, s0).
-
-maximum_dragon_glasses(3).
-%number of dragon glass
-dsJon(0 ,s0).
-
-maxX(2).
-maxY(2).
 
 isWalkable(X, Y, S):-
-    \+posObst(X, Y),
-    \+posWW(X, Y, S),
+    not(posObst(X, Y)),
+    not(posWW(X, Y, S)),
     maxX(MX), X =< MX,
     maxY(MY),Y =< MY,
     X >= 0, Y >= 0.
 	
-haveDs(S) :-
+haveDs(X, Y, C, S) :-
 	posJon(X, Y, C, S),
 	C > 0.
 	
 
 %Successor state of posJon
+
 posJon(X, Y, C, result(A, S)):-
     (
 		(	
@@ -60,16 +48,18 @@ posJon(X, Y, C, result(A, S)):-
 			)
 		);
 		(
+			
 			C1 is C+1,
-			posJon(X, Y, C1, S),
-			A = killWW,
-			haveDs(S),
+			haveDs(X, Y, C1, S),
+			A = killWW,	
 			(
 				X2 is X+1, posWW(X2, Y, S);
 				X1 is X-1, posWW(X1, Y, S);
 				Y2 is Y-1, posWW(X, Y2, S);
 				Y1 is Y+1, posWW(X, Y1, S)
-			)
+			),
+			
+			posJon(X, Y, C1, S)
 		)
     ).
 
@@ -78,75 +68,87 @@ posJon(X, Y, C, result(A, S)):-
 posWW(X, Y, result(A, S)):-
 	posWW(X, Y, S),
 	(
-		(A \= killWW);
+		
 		(
-			A = killWW,
-			X1 is X-1, \+posJon(X1, Y, C, S),
-			X2 is X+1, \+posJon(X2, Y, C, S),
-			Y1 is Y-1, \+posJon(X, Y1, C, S),
-			Y2 is Y+1, \+posJon(X, Y2, C, S)
-		);
-		(
-			A = killWW,
+			A \= killWW,
+			(
+				(X1 is X-1, \+posJon(X1, Y, C, S));
+				(X2 is X+1, \+posJon(X2, Y, C, S));
+				(Y1 is Y-1, \+posJon(X, Y1, C, S));
+				(Y2 is Y+1, \+posJon(X, Y2, C, S))
+			);
 			(
 				(X1 is X-1, posJon(X1, Y, C, S));
 				(X2 is X+1, posJon(X2, Y, C, S));
 				(Y1 is Y-1, posJon(X, Y1, C, S));
 				(Y2 is Y+1, posJon(X, Y2, C, S))
-			),
-			\+haveDs(S)
+			)
+		);
+		
+		(
+			A = killWW,
+			(
+				(X1 is X-1, \+posJon(X1, Y, C, S)),
+				(X2 is X+1, \+posJon(X2, Y, C, S)),
+				(Y1 is Y-1, \+posJon(X, Y1, C, S)),
+				(Y2 is Y+1, \+posJon(X, Y2, C, S))
+			)
+		);
+		(
+			A = killWW,
+			(
+				(X1 is X-1, posJon(X1, Y, C, S), \+haveDs(X1, Y, C, S));
+				(X2 is X+1, posJon(X2, Y, C, S), \+haveDs(X2, Y, C, S));
+				(Y1 is Y-1, posJon(X, Y1, C, S), \+haveDs(X, Y1, C, S));
+				(Y2 is Y+1, posJon(X, Y2, C, S), \+haveDs(X, Y2, C, S))
+			)
 		)
 	).
 	
 	
-killedWW(X, Y, s0).
-killedWW(X, Y, result(A, S)):-
-	killedWW(X, Y, S),
-	\+posWW(X, Y, S),
-	(
-	A = right;
-	A = left;
-	A = killWW;
-	A = up;
-	A = down
-	).
 
 killedWW2(X, Y, S) :-
-	not(posWW(X, Y, S)).		
+not(posWW(X, Y, S)).		
+
+
+
+posObst(1, 1).
+posObst(0, 1).
+posDS(2, 1).
+
+posJon(2, 2, 0, s0).
+
+maximum_dragon_glasses(3).
+
+maxX(2).
+maxY(2).
+
+posWW(0, 0, s0).
+
+
+
+
+
 	
 	
+
+
+
+
 	
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-goalTest(X, Y, S):-
-	open('file.txt',write, Stream),
-	forall(posWW(X, Y, S), write(Stream,posWW(X, Y, S))),
-	close(Stream).
-	
-readfacts:-
-    open('file.txt',read,In),
-    repeat,
-    read_line_to_codes(In,X),writef(" "),
-	writef(""),nl,
-    writef(X),nl,
-    X=end_of_file,!,
-    nl,
-    close(In).
-	
-   
+%killedWW(X, Y, s0).
+%killedWW(X, Y, result(A, S)):-
+%	killedWW(X, Y, S),
+%	\+posWW(X, Y, S),
+%	(
+%	A = right;
+%	A = left;
+%	A = killWW;
+%	A = up;
+%	A = down
+%	).
+  
 %Successor state of dsJon
 % dsJon(X, result(A, S)) :-
 % 	(	
